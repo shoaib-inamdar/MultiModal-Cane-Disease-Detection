86a7381b0b28591cf5c3d11142a8740b3b2f22b6
Priyanka Kumbhar <priyankakumbhar.2405@gmail.com>
2026-09-24 16:27:19 +0530

    feat : implement synthetic SugarcaneDataset and DataLoader pipeline

---
+++ src/data/augmentation.py
@@ -0,0 +1,35 @@
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+
+def get_train_tranforms(image_size:int =224)->A.Compose:
+    return A.Compose([
+        A.Resize(image_size,image_size),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.2),
+        A.Rotate(limit=30,p=0.5),
+        A.ColorJitter(
+            brightness=0.2,
+            contrast=0.2,
+            saturation=0.2,
+            hue=0.05,
+            p=0.2
+        ),
+        A.CLAHE(clip_limit=2.0,tile_grid_size=(8,8),p=0.3),
+        A.GaussianBlur(blur_limit=(3,5),p=0.2),
+        A.Normalize(
+            mean=[0.485,0.456,0.406],
+            std=[0.229,0.224,0.225]
+             ),
+        ToTensorV2()
+    ])
+def _val_transforms(image_size:int =224)->A.Compose:
+    return A.Compose([
+        A.Resize(image_size,image_size),
+        A.Normalize(
+            mean=[0.485,0.456,0.406],
+            std=[0.229,0.224,0.225]
+            ),
+        ToTensorV2()
+        
+    ])
